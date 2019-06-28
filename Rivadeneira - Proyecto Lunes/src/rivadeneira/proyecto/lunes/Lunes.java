1efbc2089293c07a672fc0a4f353082827f1b19a
jose luis <jose luis@DESKTOP-BF4TAK6>
2019-06-28 19:41:16 -0300

    Agregamos un como estas

--- Rivadeneira - Proyecto Lunes/src/rivadeneira/proyecto/lunes/Lunes.java
+++ Rivadeneira - Proyecto Lunes/src/rivadeneira/proyecto/lunes/Lunes.java
@@ -17,6 +17,7 @@ public class Lunes {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println ("Hola mundo");
+        System.out.println ("Como estas");
     }
     
 }
