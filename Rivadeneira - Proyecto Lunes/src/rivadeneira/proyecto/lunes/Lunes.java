2450b6da34d3ee60c1a70e34262a853327a3400b
jose luis <jose luis@DESKTOP-BF4TAK6>
2019-06-28 20:18:39 -0300

    Agregamos un ventana de login

--- Rivadeneira - Proyecto Lunes/src/rivadeneira/proyecto/lunes/Lunes.java
+++ Rivadeneira - Proyecto Lunes/src/rivadeneira/proyecto/lunes/Lunes.java
@@ -5,19 +5,29 @@
  */
 package rivadeneira.proyecto.lunes;
 
+
 /**
  *
  * @author jose luis
  */
 public class Lunes {
-
+public static VentanaLogin ventanaLogin = null;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+        ventanaLogin = new VentanaLogin();
+
         System.out.println ("Hola mundo");
         System.out.println ("Como estas");
+        
+       iniciar();
+      
+       
     }
-    
+     public static void iniciar(){
+          ventanaLogin.setVisible(true);   
+        }
 }
